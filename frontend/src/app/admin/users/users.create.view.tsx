fdae37fd85126b3c1b932464d01f8474c03bf2b0
yudhaginongpratidina <dev.yudhaginongpratidina.com>
2025-01-06 07:41:30 +0700

    6.1.25.1 - modify link back and add button link

--- frontend/src/app/admin/users/users.create.view.tsx
+++ frontend/src/app/admin/users/users.create.view.tsx
@@ -5,9 +5,9 @@ import Input from "@/components/ui/Input"
 
 import { createUserFormSchema, CreateUserFormSchema } from "./users.validator";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 import { IoCaretBackSharp } from "react-icons/io5";
 
@@ -42,15 +42,19 @@ export default function UsersCreate() {
         }
     }
 
+    const backPage = () => {
+        window.history.back()
+    }
+
     return (
         <AdminLayout>
             <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div className="w-full">
                     <h1 className="text-2xl font-semibold capitalize">create user</h1>
-                    <h2 className="text-md font-medium text-gray-600">create a new user</h2>
+                    <h2 className="text-md font-medium text-gray-600">Create a new user</h2>
                 </div>
                 <div className="w-full flex items-center justify-start md:justify-end gap-2.5">
-                    <button className="w-full md:w-fit px-6 py-1.5 border rounded-md flex items-center justify-center gap-2.5 bg-gray-950 hover:bg-gray-800 text-white duration-100">
+                    <button onClick={backPage} className="w-full md:w-fit px-6 py-1.5 border rounded-md flex items-center justify-center gap-2.5 bg-gray-950 hover:bg-gray-800 text-white duration-100">
                         <IoCaretBackSharp className="w-4 h-4" />
                         <span className="font-medium capitalize">back</span>
                     </button>
--- frontend/src/components/partials/admin/SidebarAdmin.tsx
+++ frontend/src/components/partials/admin/SidebarAdmin.tsx
@@ -1,8 +1,10 @@
-// ICONS
+import { Link, useLocation } from "react-router-dom";
+
 import { FaFire } from "react-icons/fa6";
 import { ImMenu } from "react-icons/im";
+import { FaUsers } from "react-icons/fa";
 
-export default function SidebarAdmin({onClick}: {onClick: () => void}) {
+export default function SidebarAdmin({ onClick }: { onClick: () => void }) {
     return (
         <div className="w-full fixed top-0 z-10 max-w-xs min-h-screen border-r bg-white">
 
@@ -17,6 +19,31 @@ export default function SidebarAdmin({onClick}: {onClick: () => void}) {
                 </button>
             </div>
 
+            {/* LINKS */}
+            <div className="w-full p-4 flex flex-col gap-2.5">
+                <ButtonLink 
+                    name="users management" 
+                    to="/users"
+                    icon={<FaUsers className="w-5 h-5" />}
+                />
+            </div>
+
+
         </div>
+    )
+}
+
+const ButtonLink = ({ name, to, icon }: { name: string, to: string, icon: React.ReactNode }) => {
+
+    const location = useLocation();
+    const active = location.pathname;
+
+    return (
+        <Link to={to}>
+            <button className={`w-full py-1.5 px-4 border border-transparent hover:rounded-md flex items-center gap-2.5 hover:bg-purple-200 ${active === to ? "bg-purple-500 hover:bg-purple-600 text-white rounded-md" : ""}`}>
+                {icon}
+                <span className="capitalize font-semibold">{name}</span>
+            </button>
+        </Link>
     )
 }
